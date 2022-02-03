187aa7d21bfec05bf89df6552519fdd0f371515b
skyler <sjgoh@ucsd.edu>
2022-02-02 16:17:30 -0800

    copy joes code

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,7 +14,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testFile1() throws IOException {
-        String contents= Files.readString(Path.of("C:/Users/skyle/Documents/GitHub/markdown-parse/test-file.md"));
+        String contents= Files.readString(Path.of("test-file.md"));
         List<String> expect = List.of("https://something.com", "some-page.html");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }  
